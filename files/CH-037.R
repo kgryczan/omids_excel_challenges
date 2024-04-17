10726e769a3937ed31ab00ca2eac36f224d7f906
kgryczan <kgryczan@gmail.com>
2024-04-17 17:34:47 +0200

    small fix

--- files/CH-037.R
+++ files/CH-037.R
@@ -8,10 +8,11 @@ test  = read_excel("files/CH-037 Connected people.xlsx", range = "E2:F6")
 result = input %>%
   graph_from_data_frame(directed = FALSE) %>%
   components() %>%
-  membership() %>%
-  as_tibble() %>%
+  membership()  %>%
+  as.data.frame() %>%
   rownames_to_column("name") %>%
   summarise(People = str_c(sort(as.numeric(name)), collapse = ","), .by = "x")
 
 print(result)
 print(test)
+
