4e603d6a34d9ad12e7aaef891763478b392a1f7b
kgryczan <kgryczan@gmail.com>
2025-02-12 06:05:27 +0100

    CH-188 Uploaded

---
+++ files/CH-188.R
@@ -0,0 +1,8 @@
+create_matrix <- function(n) diag(1, n, n)
+
+create_matrix(3)
+#       [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    0    1    0
+# [3,]    0    0    1
+
---
+++ files/CH-188.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+def create_matrix(n):
+    return np.eye(n)
+
+print(create_matrix(3))
